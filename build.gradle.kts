9b3c3588c8da5e0de7385788f5bb22a8729ffbcc
Victor Shpyrka <victor.shpyrka@gmail.com>
2025-02-24 10:21:01 +0200

    chore: Fixed name modules spacing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.views"
+    namespace = "com.example.wrapchildrengruop"
     compileSdk = sdk.versions.compileSdk.get().toInt()
 
     defaultConfig {
